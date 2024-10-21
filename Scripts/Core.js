a57a26bbd118f368a1b23016a8922a2727a6283a
Kane Adams <55718634+KaneAdams-Dev@users.noreply.github.com>
2024-10-21 13:42:39 +0100

    Collapsible content test
    Testing how to collapse content

--- Scripts/Core.js
+++ Scripts/Core.js
@@ -21,3 +21,20 @@ function goFullscreen(a_id){
         element.webkitRequestFullScreen();
     }
 }
+
+function expandCollapsible(){
+    var coll = documnt.getElementByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++){
+        coll[i].addEventListener("click", function(){
+            this.classList.toggle("active");
+            var content = this.nectElementSibling;
+            if (content.style.display === "block"){
+                content.style.display = "none";
+            }else{
+                content.style.display = "block";
+            }
+        });
+    }
+}
