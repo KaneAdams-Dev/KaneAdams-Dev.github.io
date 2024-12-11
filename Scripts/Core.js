7bb66761e13a882cb4f5b6357d8d4ea64e72bcd7
Kane Adams <55718634+KaneAdams-Dev@users.noreply.github.com>
2024-12-11 23:17:33 +0000

    Terrain Generation
    Added new Terrain Generation page and started adding options for Embedded WebGL games

--- Scripts/Core.js
+++ Scripts/Core.js
@@ -46,3 +46,17 @@ function expandCollapsibles(a_coll){
         content.style.maxHeight = content.scrollHeight + "px";
     }
 }
+
+function copyToClipboard(a_stringToCopy){
+    var copyText = document.getElementById(a_stringToCopy);
+
+    // copyText.select();
+    // copyText.setSelectRange(0, 99999);  // For mobile devices
+
+    var gameUrl = copyText.src; // Get the URL of the iframe
+        navigator.clipboard.writeText(gameUrl).then(() => {
+            alert('Game link copied to clipboard!');
+        }).catch(err => {
+            console.error('Failed to copy text: ', err);
+        });
+}
