f3558d2429ef8f544085802ac064da943210726e
KaneAdams-Dev <55718634+KaneAdams-Dev@users.noreply.github.com>
2025-01-22 00:05:15 +0000

    Thumbnail preview attempt
    Added small videos and added a script to test on mouse hover code

---
+++ scripts/projects.js
@@ -0,0 +1,8 @@
+function PlayPreview(a_id){
+    document.getElementById("Mobbers").src = "/Videos/Mobbers/Library.mp4"
+}
+
+function size(x){
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
