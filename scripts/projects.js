c15615aa1fbbed5f08bb09727d5f3daeed2273d3
Kane Adams <55718634+KaneAdams-Dev@users.noreply.github.com>
2025-01-22 22:43:36 +0000

    Random Preview attempt
    Added function for randomly choosing a theme, but not work as discovered that JavaScript doesn't have dictionaries, but instead Objects can be used in its place (I believe)

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,37 @@
 currentThumbnail;
 
+
+
+function GetRandomPreview(a_id){
+    console.log(a_id);
+
+    const videoOptions = {
+        Mobbers: "/Images/Mobbers/Lib.gif"
+    };
+    console.log(a_id in videoOptions);
+
+    let keyExist = Object.keys(videoOptions).some(key => key === a_id);
+    console.log(keyExist);
+    const gifs = videoOptions[a_id];
+
+    return videoOptions.Mobbers;//gifs ? gifs[Math.floor(Math.random() * gifs.length)] : null;
+}
+
 function PlayPreview(a_id) {
     let x = document.getElementById(a_id);
 
+    console.log(a_id);
     currentThumbnail = x.src;
-    x.src = "/Images/Mobbers/Lib.gif"
+    // if ('Mobbers' in videoOptions) {
+    //     x.src = "/Images/Mobbers/Lib.gif";
+    // }
+    let chosenGIF = GetRandomPreview(a_id);
+    //if (chosenGIF != null){
+        x.src = chosenGIF;
+    //}else {
+        //x.style.width = "10px";
+    //}
+    
 }
 
 function StopPreview(a_id) {
