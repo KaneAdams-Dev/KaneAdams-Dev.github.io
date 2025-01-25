be974d511a97a0afd772e918e8dc2c8559a5f74c
Kane Adams <55718634+KaneAdams-Dev@users.noreply.github.com>
2025-01-25 14:38:00 +0000

    Popup functionality
    Able to open and close popup with specific tyle and close button.

---
+++ Scripts/popups.js
@@ -0,0 +1,9 @@
+function OpenPopup(a_id) {
+    let card = document.getElementById(a_id);
+    card.style.display = "block";
+}
+
+function ClosePopup(a_id) {
+    let card = document.getElementById(a_id);
+    card.style.display = "none";
+}
