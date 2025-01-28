5070feb1fedbcc20df5211d1ac4245198aefa5d7
KaneAdams-Dev <55718634+KaneAdams-Dev@users.noreply.github.com>
2025-01-28 12:14:44 +0000

    Automated project cards
    Got cards able to update data using their ID as the key, and to work in a single popup.
    
    Next: autofill project grid with up to 12 projects, with all data in JavaScript

--- scripts/project-cards.js
+++ scripts/project-cards.js
@@ -8,6 +8,20 @@ const projects = {
         role: "Lead Programmer",
         size: "5-10",
         tools: "Unity, C#, GitHub",
+        timeline: "September 2022 to December 2022",
+        ProjectLink: "Projects/Mobbers.html",
+    },
+
+    Mobberville: {
+        title: "Mobberville",
+        description: "Farming game where you... play as a mob of a dungeon... ",
+        thumbnail: "/Images/Zombeez/SH_AKBoss.PNG",
+        previews: ["/Images/Mobbers/Lib.gif", "/Images/Mobbers/test.gif"],
+        summary: "Huh!",
+        role: "Lead Programmer",
+        size: "8",
+        tools: "Unreal Engine 5, C++, GitHub, Premiere Pro",
+        timeline: "June 2023 to August 2023",
         ProjectLink: "Projects/Mobbers.html",
     }
 };
@@ -16,9 +30,17 @@ currentThumbnail;
 currentImageID;
 
 function PopulateProjects() {
+    let projectCount = 0;
     Object.keys(projects).forEach(projectID => {
-        ApplyProjectData(projectID);
-        console.log(projectID);
+        if (projectCount >= 12){
+            console.warn("Maximum number of projects reached");
+            return;
+        }
+
+        let isAdded = ApplyProjectData(projectID);
+        if (isAdded){
+            projectCount++;
+        }
     });
 }
 
@@ -27,13 +49,13 @@ function ApplyProjectData(a_id) {
     if (!project) {
         console.error(`ProjectLink ' {
             childElement.href = project[key];${a_id}' not found.`);
-        return
+        return false;
     }
 
     const projectElement = document.getElementById(a_id);
     if (!projectElement) {
         console.error(`Element with ID '${a_id}'not found.`);
-        return;
+        return false;
     }
 
     Object.keys(project).forEach(key => {
@@ -49,6 +71,8 @@ function ApplyProjectData(a_id) {
             }
         }
     });
+
+    return true;
 }
 
 function GetRandomPreview(a_projectID) {
@@ -56,7 +80,6 @@ function GetRandomPreview(a_projectID) {
         const clips = projects[a_projectID].previews;
 
         return clips[Math.floor(Math.random() * clips.length)];
-
     } else {
         console.error(`'${a_projectID}' does not exist in projects`);
     }
@@ -65,11 +88,9 @@ function GetRandomPreview(a_projectID) {
 function PlayPreview(a_projectID) {
     const projectCard = document.getElementById(a_projectID);
     currentImageID = projectCard.querySelector('.thumbnail');
-
     currentThumbnail = currentImageID.src;
 
     let chosenClip = GetRandomPreview(a_projectID);
-
     if (!chosenClip) {
         console.error("No clip chosen.");
         return;
@@ -83,16 +104,30 @@ function StopPreview() {
 }
 
 function OpenPopup(a_id) {
-    let card = document.getElementById(a_id);
+    let card = document.getElementById('Popup');
     card.style.display = "block";
-    UpdatePopup(card);
+    UpdatePopup(card, a_id);
 }
 
-function ClosePopup(a_id) {
-    let card = document.getElementById(a_id);
+function ClosePopup() {
+    let card = document.getElementById('Popup');
     card.style.display = "none";
 }
 
-function UpdatePopup(a_card){
-    
+function UpdatePopup(a_card, a_projectID) {
+    let breakdown = a_card.querySelector('.breakdown');
+
+    breakdown.innerHTML = `
+    <b>Summary: </b> ${projects[a_projectID].summary}
+    <br><br>
+    <b>Role: </b> ${projects[a_projectID].role}
+    <br><br>
+    <b>Team Size: </b> ${projects[a_projectID].size}
+    <br><br>
+    <b>Tools: </b> ${projects[a_projectID].tools}
+    <br><br>
+    <b>Timeline: </b> ${projects[a_projectID].timeline}
+    <br><br>
+    <a href="${projects[a_projectID].ProjectLink}"><button>Got To Page</button></a>
+    `;
 }
